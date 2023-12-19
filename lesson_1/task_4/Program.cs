756a0a9286b44936087a6ad1608e4f02a00ad612
Konstantin S <selin.k2@bk.ru>
2023-12-19 20:35:15 +0300

    add lesson1:task4

---
+++ lesson_1/task_4/Program.cs
@@ -0,0 +1,28 @@
+﻿int a = 21;
+int b = 3;
+int c = 5;
+int d = 3;
+int e = 1;
+
+int max = a;
+if (b>max)
+{
+    max = b;
+
+}
+if (c>max)
+{
+    max = c;
+
+}
+if (d>max)
+{
+    max = d;
+
+}
+if (e>max)
+{
+    max = e;
+
+}
+Console.WriteLine(max);
