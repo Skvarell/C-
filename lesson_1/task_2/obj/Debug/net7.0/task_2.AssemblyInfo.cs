f784cf1d413f97d4e0cc8a9a1bb4d50a80909422
Konstantin S <selin.k2@bk.ru>
2024-02-14 14:28:40 +0900

    новый лессон 3

--- lesson_1/task_2/obj/Debug/net7.0/task_2.AssemblyInfo.cs
+++ lesson_1/task_2/obj/Debug/net7.0/task_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c187d9ce89eaa1bf9a9959204bd28b0d7512c9b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc16655c5f2fdb2719b9d6bdabae26b06ea8b1b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("task_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
