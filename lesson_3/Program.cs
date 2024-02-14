a02bb848cd1db71147ec7122a54c90e03de0443e
Konstantin S <selin.k2@bk.ru>
2024-02-14 14:44:08 +0900

    add lesson3

--- lesson_3/Program.cs
+++ lesson_3/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿double CalcilateFormula(int a, int b, int c, int d)
+{
+    double numinator = a * b;
+    int denominator = c + d;
+    double result = numinator / denominator;
+    return result;
+}
+
+double result = CalcilateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
