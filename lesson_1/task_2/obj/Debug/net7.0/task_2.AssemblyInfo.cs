f92afc960c8cae4dfa475181bb798f000b1c3ed5
Konstantin S <selin.k2@bk.ru>
2024-04-03 23:31:55 +0900

    fixed lesson_1

--- lesson_1/task_2/obj/Debug/net7.0/task_2.AssemblyInfo.cs
+++ lesson_1/task_2/obj/Debug/net7.0/task_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73cad0dd4859f0e9c05aca420c4f9dc796b33461")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f66502690d80cb29886caca88ac34ee103b33953")]
 [assembly: System.Reflection.AssemblyProductAttribute("task_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
